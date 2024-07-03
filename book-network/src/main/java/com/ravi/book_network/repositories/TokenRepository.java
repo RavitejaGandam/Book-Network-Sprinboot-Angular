942da3467bf2b5b6ec4b68a535612b7323f9ac73
Raviteja Gandam <gandamraviteja@gmail.com>
2024-07-03 15:00:04 +0530

    Added JWT

---
+++ book-network/src/main/java/com/ravi/book_network/repositories/TokenRepository.java
@@ -0,0 +1,12 @@
+package com.ravi.book_network.repositories;
+
+import com.ravi.book_network.entity.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface TokenRepository extends JpaRepository<Token,Integer> {
+
+    Optional<Token> findByToken(String token);
+}
