8de9849114431974ffb60b854f0854da57e10d16
Raviteja Gandam <gandamraviteja@gmail.com>
2024-07-29 16:54:00 +0530

    Activate and codeinput

--- book-netwrok-ui/book-network/src/app/pages/register/register.component.ts
+++ book-netwrok-ui/book-network/src/app/pages/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { RegistrationRequest } from './../../services/models/registration-request';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/services';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registerRequest : RegistrationRequest = {email:"",firstName:"",lastName:"",password:""};
+  errorMsg:Array<string> = [];
+  constructor(
+    private router :Router,
+    private authService : AuthenticationService
+  ){
+
+  }
+
+  register(){
+    this.errorMsg=[]
+    this.authService.register(
+      {
+        body:this.registerRequest
+      }
+    ).subscribe({
+      next:()=>{
+        this.router.navigate(["activate-account"]);
+      },
+      error:(err)=>{
+        this.errorMsg = err.error.validationErrors;
+      }
+    })
+
+  }
+  login(){
+    this.router.navigate(["login"]);
+  }
 
 }
