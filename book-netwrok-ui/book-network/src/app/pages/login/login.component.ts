1aa9c8ceb238b8373d71f9ce6061fcc972ec18a9
Raviteja Gandam <gandamraviteja@gmail.com>
2024-07-29 15:22:08 +0530

    CORS

--- book-netwrok-ui/book-network/src/app/pages/login/login.component.ts
+++ book-netwrok-ui/book-network/src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 import { AuthenticationRequest } from 'src/app/services/models';
+import { AuthenticationService } from 'src/app/services/services';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +12,29 @@ export class LoginComponent {
   authRequest : AuthenticationRequest={email:' ',password:''};
   errorMsg : Array<string>=[];
 
-  login(){
+  constructor(
+    private router : Router,
+    private authService : AuthenticationService
+  ){
 
   }
+
+  login(){
+    this.errorMsg = [];
+    this.authService.authenticate(
+      {
+        body:this.authRequest
+      }
+    ).subscribe({
+      next:()=>{
+        this.router.navigate(["books"]);
+      },
+      error:(err)=>{
+        console.log(err);
+      }
+    })
+  }
   register(){
-    
+    this.router.navigate(["register"]);
   }
 }
