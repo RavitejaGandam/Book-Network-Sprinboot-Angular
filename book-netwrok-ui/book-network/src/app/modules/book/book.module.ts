315600f12968475979fc1fcf43101e8876b93d70
Raviteja Gandam <gandamraviteja@gmail.com>
2024-08-01 16:26:35 +0530

    managebook component created

--- book-netwrok-ui/book-network/src/app/modules/book/book.module.ts
+++ book-netwrok-ui/book-network/src/app/modules/book/book.module.ts
@@ -8,6 +8,7 @@ import { BooksListComponent } from './pages/books-list/books-list.component';
 import { BookCardComponent } from './components/book-card/book-card.component';
 import { RatingComponent } from './components/rating/rating.component';
 import { MyBooksComponent } from './pages/my-books/my-books.component';
+import { ManageBookComponent } from './pages/manage-book/manage-book.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { MyBooksComponent } from './pages/my-books/my-books.component';
     BooksListComponent,
     BookCardComponent,
     RatingComponent,
-    MyBooksComponent
+    MyBooksComponent,
+    ManageBookComponent
   ],
   imports: [
     CommonModule,
