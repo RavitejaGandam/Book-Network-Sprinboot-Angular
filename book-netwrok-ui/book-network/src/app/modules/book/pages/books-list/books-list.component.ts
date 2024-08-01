03dbb7122aa2e2b406eb9f43492b901bf42b23a5
Raviteja Gandam <gandamraviteja@gmail.com>
2024-08-01 15:46:06 +0530

    Borrow feature implemented

--- book-netwrok-ui/book-network/src/app/modules/book/pages/books-list/books-list.component.ts
+++ book-netwrok-ui/book-network/src/app/modules/book/pages/books-list/books-list.component.ts
@@ -1,77 +1,87 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { BookResponse, PageResponseBookResponse } from 'src/app/services/models';
+import {
+  BookResponse,
+  PageResponseBookResponse,
+} from 'src/app/services/models';
 import { BookService } from 'src/app/services/services';
 
 @Component({
   selector: 'app-books-list',
   templateUrl: './books-list.component.html',
-  styleUrls: ['./books-list.component.scss']
+  styleUrls: ['./books-list.component.scss'],
 })
 export class BooksListComponent implements OnInit {
-displayBookDetails($event: BookResponse) {
-throw new Error('Method not implemented.');
-}
-borrowBook($event: BookResponse) {
-throw new Error('Method not implemented.');
-}
-  bookResponse:PageResponseBookResponse={}
+  bookResponse: PageResponseBookResponse = {};
   page: number = 0;
   size: number = 5;
   pages = [];
-  constructor(
-    private booksService: BookService,
-    private router: Router
-  ){
-
-  }
+  message = ' ';
+  level: string = 'success';
+  constructor(private booksService: BookService, private router: Router) {}
   ngOnInit(): void {
     this.findAllBooks();
     throw new Error('Method not implemented.');
   }
   findAllBooks() {
-    this.booksService.findAllBooks(
-      {
-      page:this.page,
-      size:this.size
-    }
-    ).subscribe(
-      {
-        next:(books)=>{
-          this.bookResponse=books;
-          console.log('ok');  
-        }
-      }
-    )
+    this.booksService
+      .findAllBooks({
+        page: this.page,
+        size: this.size,
+      })
+      .subscribe({
+        next: (books) => {
+          this.bookResponse = books;
+          console.log('ok');
+        },
+      });
     throw new Error('Method not implemented.');
   }
 
-  goToFirstPage(){
+  goToFirstPage() {
     this.page = 0;
     this.findAllBooks();
   }
 
-  goToPreviousPage(){
+  goToPreviousPage() {
     this.page--;
     this.findAllBooks();
   }
-  goToPage(page:number){
+  goToPage(page: number) {
     this.page = page;
     this.findAllBooks();
   }
 
-  goToNextPage(){
+  goToNextPage() {
     this.page++;
     this.findAllBooks();
   }
-  goToLastPage(){
-    this.page = this.bookResponse.totalPages as number -1;
+  goToLastPage() {
+    this.page = (this.bookResponse.totalPages as number) - 1;
     this.findAllBooks();
   }
-  get isLastPage(){
-    return this.page = this.bookResponse.totalPages as number -1;
+  get isLastPage() {
+    return (this.page = (this.bookResponse.totalPages as number) - 1);
   }
 
-
-
+  displayBookDetails($event: BookResponse) {
+    throw new Error('Method not implemented.');
+  }
+  borrowBook(book: BookResponse) {
+    this.message = ' ';
+    this.booksService
+      .borrowBook({
+        'book-id': book.id as number,
+      })
+      .subscribe({
+        next: () => {
+          this.level = 'success';
+          this.message = 'Book borrowed successfully';
+        },
+        error: (error) => {
+          this.level = 'error';
+          this.message = error.error.error;
+        },
+      });
+  }
 }
