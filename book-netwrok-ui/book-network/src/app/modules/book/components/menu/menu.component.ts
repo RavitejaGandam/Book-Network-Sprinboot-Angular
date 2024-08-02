1f8a4452ff18715b20ad177c75ad246c4c182e20
Raviteja Gandam <gandamraviteja@gmail.com>
2024-08-02 12:56:02 +0530

    Logout implemented

--- book-netwrok-ui/book-network/src/app/modules/book/components/menu/menu.component.ts
+++ book-netwrok-ui/book-network/src/app/modules/book/components/menu/menu.component.ts
@@ -3,24 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
   ngOnInit(): void {
-    const linkColor:NodeListOf<Element>=document.querySelectorAll('.nav-link')
-    linkColor.forEach(link => {
+    const linkColor: NodeListOf<Element> =
+      document.querySelectorAll('.nav-link');
+    linkColor.forEach((link) => {
       if (window.location.href.endsWith(link.getAttribute('href') || '')) {
         link.classList.add('active');
       }
       link.addEventListener('click', () => {
-        linkColor.forEach(l => l.classList.remove('active'));
+        linkColor.forEach((l) => l.classList.remove('active'));
         link.classList.add('active');
       });
     });
     throw new Error('Method not implemented.');
   }
-  logout(){
-
+  logout() {
+    localStorage.removeItem('token');
+    window.location.reload();
   }
-
 }
